985e87db3b10cb51e9198f9746a5085b43eb6fdd
donado10 <adamscoulibaly10@gmail.com>
2024-10-27 22:07:47 +0000

    update

--- src/Components/About/AboutFAQSection.tsx
+++ src/Components/About/AboutFAQSection.tsx
@@ -78,12 +78,90 @@ const AboutFAQSectionSmall = () => {
     </section>
   );
 };
+const AboutFAQSectionBig = () => {
+  const Template: React.FC<{ title: string; description: string }> = ({
+    title,
+    description,
+  }) => {
+    const [toggleResponse, setToggleResponse] = useState<boolean>(false);
+
+    return (
+      <div className="flex h-fit w-[40rem] flex-col items-center justify-center gap-6 bg-scoot-light px-4 py-8">
+        <div className="flex w-full items-center justify-between gap-8">
+          <h3 className="font-primary text-xl font-semibold text-scoot-dark">
+            {title}
+          </h3>
+          <button onClick={() => setToggleResponse((prev) => !prev)}>
+            <span>
+              <Image src={ChevronImg} alt="chevron" />
+            </span>
+          </button>
+        </div>
+        {toggleResponse && (
+          <div>
+            <p className="font-secondary text-base text-scoot-dark">
+              {description}
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <section className="my-8 flex flex-col gap-8">
+      <div>
+        <h1 className="text-center font-primary text-3xl font-bold text-scoot-dark">
+          FAQs
+        </h1>
+      </div>
+      <div>
+        <div className="flex justify-center gap-16">
+          <h2 className="text-center font-primary text-2xl font-bold text-scoot-dark">
+            How it works
+          </h2>
+          <div className="flex flex-col items-center gap-4 px-8">
+            <Template
+              title="How do I download the app?"
+              description="To download the Scoot app, you can search “Scoot” in both the App and Google Play stores. An even simpler way to do it would be to click the relevant link at the bottom of this page and you’ll be re-directed to the correct page."
+            />
+            <Template title="Can I find a nearby Scoots?" description="" />
+            <Template title="Do I need a license to ride?" description="" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-center gap-16">
+          <h2 className="text-center font-primary text-2xl font-bold text-scoot-dark">
+            Safe driving
+          </h2>
+          <div className="flex flex-col items-center gap-4 px-8">
+            <Template
+              title="Should I wear an helmet?"
+              description="Yes, please do! All cities have different laws. But we strongly strongly strongly recommend always wearing a helmet regardless of the local laws. We like you and we want you to be as safe as possible while Scooting."
+            />
+            <Template
+              title="How about the rules and regulations?"
+              description=""
+            />
+            <Template title="What if I damage my scoot?" description="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 const AboutFAQSection = () => {
   const mobileScreen = useMediaQuery(EMediaQuery.MOBILE);
-  const tabletScreen = useMediaQuery(EMediaQuery.SMALL);
+  const bigScreen = useMediaQuery(EMediaQuery.SMALL);
 
-  return <>{mobileScreen && !tabletScreen && <AboutFAQSectionSmall />}</>;
+  return (
+    <>
+      {mobileScreen && !bigScreen && <AboutFAQSectionSmall />}
+      {bigScreen && <AboutFAQSectionBig />}
+    </>
+  );
 };
 
 export default AboutFAQSection;
--- src/Components/About/AboutValuesSection.tsx
+++ src/Components/About/AboutValuesSection.tsx
@@ -53,12 +53,68 @@ const AboutValueSectionSmall = () => {
     </section>
   );
 };
+const AboutValueSectionBig = () => {
+  const Template: React.FC<{
+    image: string;
+    title: string;
+    text: string;
+  }> = ({ image, title, text }) => {
+    return (
+      <div className="flex flex-col items-center justify-center gap-8">
+        <div className="aspect-square w-[100px] overflow-hidden rounded-full">
+          <Image src={image} alt="locate" width="200" height="200" />
+        </div>
+        <div>
+          <h1 className="text-center font-primary text-3xl font-bold text-scoot-dark">
+            {title}
+          </h1>
+        </div>
+        <div className="w-[20rem]">
+          <p className="text-center font-secondary text-base font-normal leading-7 text-scoot-dim">
+            {text}
+          </p>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <section className="flex flex-col gap-16 p-8">
+      <div>
+        <h1 className="text-center font-primary text-3xl font-bold text-scoot-dark">
+          Our Values
+        </h1>
+      </div>
+      <div className="flex items-center justify-center gap-4">
+        <Template
+          image="/assets/images/our-tech.jpg"
+          title="Our tech"
+          text="We’re using cutting edge technology to drive accessible urban transportation forward. Our fully electric scooters are a joy to ride!"
+        />
+        <Template
+          image="/assets/images/our-integrity.jpg"
+          title="Our integrity"
+          text="We are fully committed to deliver a great yet safe, sustainable micro-mobility experience in every city we serve."
+        />
+        <Template
+          image="/assets/images/our-community.jpg"
+          title="Our community"
+          text="We support every community we serve. All workers are paid a living wage based on their location and are Scoot employees."
+        />
+      </div>
+    </section>
+  );
+};
 
 const AboutValueSection = () => {
   const mobileScreen = useMediaQuery(EMediaQuery.MOBILE);
   const tabletScreen = useMediaQuery(EMediaQuery.SMALL);
   const bigScreen = useMediaQuery(EMediaQuery.BIG);
-  return <>{mobileScreen && !tabletScreen && <AboutValueSectionSmall />}</>;
+  return (
+    <>
+      {mobileScreen && !bigScreen && <AboutValueSectionSmall />}
+      {bigScreen && <AboutValueSectionBig />}
+    </>
+  );
 };
 
 export default AboutValueSection;
